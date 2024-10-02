8a2169eb63cc0ba7e734e29cf6f84554457b07a5
gyupp <kkb75281@gmail.com>
2024-10-02 18:30:08 +0900

    Edit tech page

--- src/views/Tech.vue
+++ src/views/Tech.vue
@@ -34,54 +34,47 @@ let techs = [
         img: "aicity.jpg",
         tit: "AI City",
         link: "ai",
-        desc: "Visual Link has announced a groundbreaking project centered around the use of carbon semiconductors and their advanced functionalities, such as Reusable Chip, Memory-Mapped CPU, Content-Aware Memory, and Emulated Quantum Communication. This innovative project, called AI City, features a state-of-the-art datacenter campus that supports groundbreaking AI applications using cutting-edge U.S. technologies. A special group called the West Virginia Datacenter Group oversees the project. As part of the initial phase, the West Virginia State government has approved the deployment of a robust fiber optic network across the state that will connect directly to the world's primary Internet peering point. By connecting the datacenter campus and extending to every county in West Virginia, this network will enhance broadband accessibility and reduce backhaul bandwidth costs."
+        desc: "Visual Link has announced a groundbreaking project leveraging advanced functionalities of carbon semiconductors, including the Reusable Chip, Memory-Mapped CPU, Content-Aware Memory, and Emulated Quantum Communication. Named AI City, this innovative project features a state-of-the-art data center campus supporting cutting-edge AI applications using U.S. technologies. The West Virginia Datacenter Group oversees the project."
     },
     {
         img: "chip.jpg",
         tit: "Chip Subscription Business",
         link: "chip",
-        desc: "Due to its ability to generate consistent and recurring revenue, the subscription business model has gained widespread adoption across various industries, allowing for better revenue forecasting and streamlined planning. Subscriptions facilitate upselling and cross-selling by encouraging ongoing relationships with customers and promoting loyalty. It offers convenience, personalized experiences, and added value for both businesses and consumers, resulting in a trillion-dollar subscription economy."
+        desc: "The subscription business model has gained widespread adoption across various industries due to its ability to generate consistent and recurring revenue, allowing for better revenue forecasting and streamlined planning."
     },
     {
         img: "memory.jpg",
         tit: "Content Aware Memory",
         link: "memory",
-        desc: "The first semiconductor memory chips were produced by Texas Instruments in the early 1960s, followed by Intel's introduction of SRAM and DRAM chips in 1969 and 1970. Since their adoption, the fundamental memory architecture has remained largely unchanged. In today's computing environment, however, massive memory demands, such as machine learning, require many millions of addresses, consuming a lot of CPU processing power."
+        desc: "The semiconductor memory chip was pioneered by Texas Instruments in the early 1960s, followed by Intel's introduction of SRAM and DRAM chips in 1969 and 1970. Since their inception, the foundational architecture of memory has seen minimal evolution."
     }
     ,
     {
         img: "cpu.jpg",
         tit: "Memory Mapped CPU",
         link: "cpu",
-        desc: "An average CPU consists of three primary functional components: a memory management unit (MMU), a control unit, and an arithmetic logic unit (ALU). When loading programs or retrieving data, the MMU manages memory access across internal cache, memory chips, and storage devices. By decoding instructions, managing data flow, and coordinating component interactions, the control unit oversees instruction execution"
+        desc: "A typical CPU comprises three core functional components: a memory management unit (MMU), a control unit, and an arithmetic logic unit (ALU). The MMU oversees memory access across internal cache, memory chips, and storage devices when loading programs or retrieving data."
     }
     ,
     {
         img: "communication.jpg",
         tit: "Quantum Communication",
         link: "communication",
-        desc: "Quantum security is absolute, based on fundamental physical laws. By leveraging entangled particles such as photons, quantum communication offers unprecedented security. BB84 and other quantum key distribution protocols use the no-cloning theorem to safeguard keys even if qubits are intercepted in the process of creating shared secret keys. Implementing quantum communication infrastructure requires specialized hardware, precise environmental control, and cryogenic temperatures, posing significant technical challenges."
-    }
-    ,
-    {
-        img: "spooling.jpg",
-        tit: "Quantum Spooling",
-        link: "spooling",
-        desc: "Quantum Spooling makes sure only one device can read a network packet, regardless of how many copies there are. As an example, when User A sends data to User B, Quantum Spooling encapsulates the data in a quantum packet. Once a device reads one quantum packet, all the others become invalid. Emulated Quantum Communication (EQC) encrypts each packet uniquely, so eavesdropping isn't possible. The system alerts both sender and receiver if a packet is intercepted, keeping its integrity and making hacking virtually impossible."
+        desc: "Quantum communication offers unparalleled security by leveraging fundamental physical laws, particularly through the use of entangled particles like photons. Protocols such as BB84 exploit the no-cloning theorem to safeguard cryptographic keys, ensuring security even if intercepted during key distribution."
     }
     ,
     {
         img: "carbon.jpg",
         tit: "Reusable Carbon Semiconductors",
         link: "carbon",
-        desc: "Industrialization, economics, science, and education heavily rely on artificial intelligence (AI) and semiconductor technology. Yet, the computing power of artificial intelligence doubles every three months, exceeding Moore's Law by far. AI also consumes a substantial amount of electricity, enormous computing power, and vast storage. "
+        desc: "ndustrialization, economics, science, and education are increasingly reliant on artificial intelligence (AI) and semiconductor technology. However, the computational capabilities of AI are advancing at a pace that far exceeds Moore's Law, doubling every three months."
     }
     ,
     {
         img: "internet.jpg",
         tit: "The Super Internet",
         link: "internet",
-        desc: "Over four decades ago, the Internet emerged from ARPANET (Advanced Research Projects Agency Network), a project initiated and funded by the U.S. Department of Defense. Initially, ARPANET's developers aimed to create a network connecting a few million users at most, never anticipating that billions would eventually use the platform. ARPANET introduced two pivotal concepts: the network packet and the network protocol."
+        desc: "Over forty years ago, the Internet emerged from ARPANET (Advanced Research Projects Agency Network), a project funded by the U.S. Department of Defense. Initially intended to connect a few million users, ARPANET introduced foundational concepts like network packets and protocols. Today, billions of users rely on the Internet, which has seen minimal architectural updates despite ongoing demands for faster and more reliable connectivity."
     }
 ]
 </script>
